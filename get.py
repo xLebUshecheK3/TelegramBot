f298d058d95fe11e15590244bfef0d6c5044b9dc
xLebUshecheK3 <pospelovgrisa14@gmail.com>
2022-03-07 17:06:49 +0300

    Adding russian, geometry and fix bug

--- get.py
+++ get.py
@@ -1,14 +1,37 @@
 from requests import get
 from bs4 import BeautifulSoup
+from aiogram.types import Message
 
 
-def alg9mp(number: int) -> str:
+def alg9mp(number: int, message: Message) -> str:
     try:
-        responce = get(f"https://gdz.ru/class-9/algebra/merzlyak/{number}-nom/")
-        soup = BeautifulSoup(responce.text, 'lxml')
+        response = get(f"https://gdz.ru/class-9/algebra/merzlyak/{number}-nom/")
+        soup = BeautifulSoup(response.text, 'lxml')
         url = "https:" + str(soup.find("img", attrs={'alt': f"ГДЗ по алгебре 9 класс  Мерзляк   упражнение - {number}, Решебник к учебнику 2021"})).replace('" title=""/>', '').split('src="')[1]
         return url
     except Exception as e:
         with open("logs.txt", "a") as file:
-            file.write(f"[ERROR] {e}\n")
+            file.write(f"[ERROR] ({message.date}) - {e}\n")
+        return ""
+
+def geo9mp(number: int, message: Message) -> str:
+    try:
+        response = get(f"https://gdz.ru/class-9/geometria/merzlyak-polonskij/{number}-nom/")
+        soup = BeautifulSoup(response.text, 'lxml')
+        url = "https:" + str(soup.find("img", attrs={'alt': f"ГДЗ по геометрии 9 класс  Мерзляк   упражнение - {number}, Решебник"})).replace('" title=""/>', '').split('src="')[1]
+        return url
+    except Exception as e:
+        with open("logs.txt", "a") as file:
+            file.write(f"[ERROR] ({message.date}) - {e}\n")
+        return ""
+
+def rus9bl(number: int, message: Message) -> str:
+    try:
+        response = get(f"https://gdz.ru/class-9/russkii_yazik/barhudarov-kruchkov-9/{number}-nom/")
+        soup = BeautifulSoup(response.text, 'lxml')
+        url = "https:" + str(soup.find("img", attrs={'alt': f"ГДЗ по русскому языку 9 класс  Бархударов   упражнение - {number}, Решебник к учебнику 2019"})).replace('" title=""/>', '').split('src="')[1]
+        return url
+    except Exception as e:
+        with open("logs.txt", "a") as file:
+            file.write(f"[ERROR] ({message.date}) - {e}\n")
         return ""
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import logging
 import get
 from aiogram import Bot, Dispatcher, executor, types
 from config import TOKEN
-from time import time, gmtime
 
+# Список кортежей, хранящий id пользователя и его текущий запрос
+sessions = []
 
 # Настрока логирования
 logging.basicConfig(level=logging.INFO)
@@ -33,39 +34,105 @@ def get_keyboard() -> types.ReplyKeyboardMarkup:
 async def start(message: types.Message):
     await message.answer("Выберите предмет, для которого вы хотите найти ответы", reply_markup=get_keyboard())
 
-# Выбор класса для алгебры
+# Выбор класса для алгебры 
 @dp.message_handler(lambda message: message.text == "📘 Алгебра 📘")
 async def algebra(message: types.Message):
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(types.InlineKeyboardButton(text="9 класс", callback_data="alg9"))
     await message.answer("Выберите класс", reply_markup=keyboard)
 
-# Выбор автора учебника
+# Выбор класса для геометрии
+@dp.message_handler(lambda message: message.text == "📙 Геометрия 📙")
+async def geometry(message: types.Message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text="9 класс", callback_data="geo9"))
+    await message.answer("Выберите класс", reply_markup=keyboard)
+
+# Выбор класса для русского языка
+@dp.message_handler(lambda message: message.text == "📕 Русский язык 📕")
+async def russian(message: types.Message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text="9 класс", callback_data="rus9"))
+    await message.answer("Выберите класс", reply_markup=keyboard)
+
+# Выбор автора учебника для алгебры 9 класса
 @dp.callback_query_handler(text="alg9")
-async def author(call: types.CallbackQuery):
+async def author_alg9(call: types.CallbackQuery):
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(types.InlineKeyboardButton(text="Мерзляк, Полонский", callback_data="alg9mp"))
     await call.message.edit_text("Выберите автора", reply_markup=keyboard)
     await call.answer()
 
-# Выбор номер задания
-@dp.callback_query_handler(text="alg9mp")
-async def number(call: types.CallbackQuery):
-    await call.message.edit_text("Укажите номер задания")
+# Выбор автора учебника для геометрии 9 класса
+@dp.callback_query_handler(text="geo9")
+async def author_geo9(call: types.CallbackQuery):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text="Мезрляк, Полонский", callback_data="geo9mp"))
+    await call.message.edit_text("Выберите автора", reply_markup=keyboard)
     await call.answer()
 
-# Парсинг фотографии и отправка пользователю
+# Выбор автора учебника для русского языка 9 класса
+@dp.callback_query_handler(text="rus9")
+async def author_rus9(call: types.CallbackQuery):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text="Бархударов, Ладыженская", callback_data="rus9bl"))
+    await call.message.edit_text("Выберите автора", reply_markup=keyboard)
+    await call.answer()
+
+# Выбор номера задания для алгебры 9 класса Мерзляк, Полоснкий
+@dp.callback_query_handler(text="alg9mp")
+async def number_alg9mp(call: types.CallbackQuery):
+    await call.message.edit_text("Укажите номер задания")
+    sessions.append((call.message.chat.id, "alg9mp"))
+    await call.answer()
+
+# Выбор номера задания для геометрии 9 класса Мерзляк, Полонский
+@dp.callback_query_handler(text="geo9mp")
+async def number_geo9mp(call: types.CallbackQuery):
+    await call.message.edit_text("Укажите номер задания")
+    sessions.append((call.message.chat.id, "geo9mp"))
+    await call.answer()
+
+# Выбор номера задания для русского языка 9 класса Бархударов, Ладыженская
+@dp.callback_query_handler(text="rus9bl")
+async def number_rus9bl(call: types.CallbackQuery):
+    await call.message.edit_text("Укажите номер задания")
+    sessions.append((call.message.chat.id, "rus9bl"))
+    await call.answer()
+
+# Парсинг фотографии ответа и отправка пользователю
 @dp.message_handler(lambda message: int(message.text) in (i for i in range(1, 2000)))
-async def post(message: types.Message):
-    response = get.alg9mp(int(message.text))
-    if response:
-        await message.answer_photo(response, reply_markup=get_keyboard())
-        # Логирование
-        with open("logs.txt", "a") as file:
-            t = gmtime(time() + 10800)
-            file.write(f"[INFO] ({t.tm_mday}.{t.tm_mon}.{t.tm_year} {t.tm_hour}:{t.tm_min}:{t.tm_sec}) - {message.from_user.id}, alg9mp\n")
-    else:
-        await message.answer("Произошла неизвестная ошибка")
+async def post_alg9mp(message: types.Message):
+    if tuple([message.chat.id, "alg9mp"]) in sessions:
+        response = get.alg9mp(int(message.text), message)
+        if response:
+            await message.answer_photo(response, reply_markup=get_keyboard())
+            sessions.pop(sessions.index((message.chat.id, "alg9mp")))
+            # Логирование
+            with open("logs.txt", "a") as file:
+                file.write(f"[INFO] ({message.date}) - {message.chat.id}, alg9mp\n")
+        else:
+            await message.answer("Произошла неизвестная ошибка")
+    elif tuple([message.chat.id, "geo9mp"]) in sessions:
+        response = get.geo9mp(int(message.text), message)
+        if response:
+            await message.answer_photo(response, reply_markup=get_keyboard())
+            sessions.pop(sessions.index((message.chat.id, "geo9mp")))
+            # Логирование
+            with open("logs.txt", "a") as file:
+                file.write(f"[INFO] ({message.date}) - {message.chat.id}, geo9mp\n")
+        else:
+            await message.answer("Произошла неизвестная ошибка")
+    elif tuple([message.chat.id, "rus9bl"]) in sessions:
+        response = get.rus9bl(int(message.text), message)
+        if response:
+            await message.answer_photo(response, reply_markup=get_keyboard())
+            sessions.pop(sessions.index((message.chat.id, "rus9bl")))
+            #Логирование
+            with open("logs.txt", "a") as file:
+                file.write(f"[INFO] ({message.date}) - {message.chat.id}, rus9bl\n")
+        else:
+            await message.answer("Произошла неизвестная ошибка")
 
 
 if __name__ == '__main__':
